b5da368d1eb5412bb5d969f49e3b54a8e20ed034
ahmed509 <53703321+ahmed509@users.noreply.github.com>
2019-11-17 17:42:38 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Human from "./Animal.js";
+
+const me = new Student();
+
+me.getJob("Web developer");
+
+me.learnNewSkill("React");
+
+me.forgetSkill("React");
+
+me.leaveJob();
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
